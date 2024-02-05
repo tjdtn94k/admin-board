53c87658f79720df8ce60bf67665ca13199cd185
kimseongsu <ss.kim@okestro.com>
2024-02-05 11:23:17 +0900

    [refactor] board admin setting
    - board-admin setting

---
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/ProjectBoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.admin.board.projectboardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBoardAdminApplication.class, args);
+	}
+
+}
---
+++ project-board-admin/src/test/java/com/admin/board/projectboardadmin/ProjectBoardAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.board.projectboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
