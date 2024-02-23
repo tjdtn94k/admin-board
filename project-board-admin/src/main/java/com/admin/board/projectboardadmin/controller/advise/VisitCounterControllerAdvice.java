6748fd50058a00f382377a84b8956558479be726
kimseongsu <ss.kim@okestro.com>
2024-02-23 17:41:03 +0900

    [feature] 방문 횟수 기능 추가
    - 방문 횟수 기능 추가

---
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/advise/VisitCounterControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.admin.board.projectboardadmin.controller.advise;
+
+import com.admin.board.projectboardadmin.service.VisitCounterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@RequiredArgsConstructor
+@ControllerAdvice
+public class VisitCounterControllerAdvice {
+
+    private final VisitCounterService visitCounterService;
+
+    @ModelAttribute("visitCount")
+    public Long visitCount() {
+        return visitCounterService.visitCount();
+    }
+}
---
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/service/VisitCounterService.java
@@ -0,0 +1,41 @@
+package com.admin.board.projectboardadmin.service;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.search.MeterNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import io.micrometer.core.instrument.Timer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class VisitCounterService {
+
+    private final MeterRegistry meterRegistry;
+
+    private static final List<String> viewEndpoints = List.of(
+            "/management/articles",
+            "/management/article-comments",
+            "/management/user-accounts",
+            "/admin/members"
+    );
+
+    public long visitCount() {
+        long sum;
+
+        try {
+            sum = meterRegistry.get("http.server.requests")
+                    .timers()
+                    .stream()
+                    .filter(timer -> viewEndpoints.contains(timer.getId().getTag("uri")))
+                    .mapToLong(Timer::count)
+                    .sum();
+        } catch (MeterNotFoundException e) {
+            sum = 0L;
+        }
+
+        return sum;
+    }
+
+}
