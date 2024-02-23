2cf4c6172e7b65c3b51cd4af7d5b375de02d5755
kimseongsu <ss.kim@okestro.com>
2024-02-23 17:38:09 +0900

    [feature] User 관련 Service 기능 추가
    - User 관련 Service 기능 추가

--- project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/UserAccountManagementController.java
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/UserAccountManagementController.java
@@ -1,17 +1,40 @@
 package com.admin.board.projectboardadmin.controller;
 
+import com.admin.board.projectboardadmin.dto.response.UserAccountResponse;
+import com.admin.board.projectboardadmin.service.UserAccountManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/management/user-account")
-@RestController
+@RequiredArgsConstructor
+@RequestMapping("/management/user-accounts")
+@Controller
 public class UserAccountManagementController {
+
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
     public String userAccounts(Model model) {
-
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
 
         return "management/user-accounts";
     }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
+    }
+
 }
