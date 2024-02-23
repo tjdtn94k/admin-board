ab963e7fde9a0b5d5d209d3497aa23a1cfa2a646
kimseongsu <ss.kim@okestro.com>
2024-02-23 17:40:17 +0900

    [feature] 게시글 기능 추가
    - 게시글 기능 추가

--- project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/ArticleManagementController.java
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/ArticleManagementController.java
@@ -1,16 +1,40 @@
 package com.admin.board.projectboardadmin.controller;
 
+import com.admin.board.projectboardadmin.dto.response.ArticleResponse;
+import com.admin.board.projectboardadmin.service.ArticleManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
 @RequestMapping("/management/articles")
-@RestController
+@Controller
 public class ArticleManagementController {
+
+    private final ArticleManagementService articleManagementService;
+
     @GetMapping
     public String articles(Model model) {
-
+        model.addAttribute(
+                "articles",
+                articleManagementService.getArticles().stream().map(ArticleResponse::withoutContent).toList()
+        );
 
         return "management/articles";
     }
+
+    @ResponseBody
+    @GetMapping("/{articleId}")
+    public ArticleResponse article(@PathVariable Long articleId) {
+        return ArticleResponse.withContent(articleManagementService.getArticle(articleId));
+    }
+
+    @PostMapping("/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:/management/articles";
+    }
+
 }
