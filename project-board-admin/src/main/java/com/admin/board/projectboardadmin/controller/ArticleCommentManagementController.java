ce1846b21555dc11fbf94f8e368b15c79127a900
kimseongsu <ss.kim@okestro.com>
2024-02-23 17:40:42 +0900

    [feature] 게시글 comment 기능 추가
    - 게시글 comment 기능 추가

--- project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/ArticleCommentManagementController.java
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/controller/ArticleCommentManagementController.java
@@ -1,18 +1,40 @@
 package com.admin.board.projectboardadmin.controller;
 
+import com.admin.board.projectboardadmin.dto.response.ArticleCommentResponse;
+import com.admin.board.projectboardadmin.service.ArticleCommentManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/management/article-comment")
-@RestController
+@RequiredArgsConstructor
+@RequestMapping("/management/article-comments")
+@Controller
 public class ArticleCommentManagementController {
+
+    private final ArticleCommentManagementService articleCommentManagementService;
 
     @GetMapping
     public String articleComments(Model model) {
-
+        model.addAttribute(
+                "comments",
+                articleCommentManagementService.getArticleComments().stream().map(ArticleCommentResponse::of).toList()
+        );
 
         return "management/article-comments";
     }
+
+    @ResponseBody
+    @GetMapping("/{articleCommentId}")
+    public ArticleCommentResponse articleComment(@PathVariable Long articleCommentId) {
+        return ArticleCommentResponse.of(articleCommentManagementService.getArticleComment(articleCommentId));
+    }
+
+    @PostMapping("/{articleCommentId}")
+    public String deleteArticleComment(@PathVariable Long articleCommentId) {
+        articleCommentManagementService.deleteArticleComment(articleCommentId);
+
+        return "redirect:/management/article-comments";
+    }
+
 }
---
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/dto/response/ArticleCommentResponse.java
@@ -0,0 +1,25 @@
+package com.admin.board.projectboardadmin.dto.response;
+
+import com.admin.board.projectboardadmin.dto.ArticleCommentDto;
+import com.admin.board.projectboardadmin.dto.UserAccountDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ArticleCommentResponse(
+        Long id,
+        UserAccountDto userAccount,
+        String content,
+        LocalDateTime createdAt
+) {
+
+    public static ArticleCommentResponse of(Long id, UserAccountDto userAccount, String content, LocalDateTime createdAt) {
+        return new ArticleCommentResponse(id, userAccount, content, createdAt);
+    }
+
+    public static ArticleCommentResponse of(ArticleCommentDto dto) {
+        return ArticleCommentResponse.of(dto.id(), dto.userAccount(), dto.content(), dto.createdAt());
+    }
+
+}
