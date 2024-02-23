8c9045bd07eb4bac5d82e8109b925f1ba101b4c9
kimseongsu <ss.kim@okestro.com>
2024-02-23 17:39:31 +0900

    [feature] admin 관련 기능 추가
    - admin 관련 기능 추가

---
+++ project-board-admin/src/main/java/com/admin/board/projectboardadmin/service/AdminAccountService.java
@@ -0,0 +1,45 @@
+package com.admin.board.projectboardadmin.service;
+
+import com.admin.board.projectboardadmin.domain.AdminAccount;
+import com.admin.board.projectboardadmin.domain.constant.RoleType;
+import com.admin.board.projectboardadmin.dto.AdminAccountDto;
+import com.admin.board.projectboardadmin.repository.AdminAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class AdminAccountService {
+
+    private final AdminAccountRepository adminAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<AdminAccountDto> searchUser(String username) {
+        return adminAccountRepository.findById(username)
+                .map(AdminAccountDto::from);
+    }
+
+    public AdminAccountDto saveUser(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return AdminAccountDto.from(
+                adminAccountRepository.save(AdminAccount.of(username, password, roleTypes, email, nickname, memo, username))
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<AdminAccountDto> users() {
+        return adminAccountRepository.findAll().stream()
+                .map(AdminAccountDto::from)
+                .toList();
+    }
+
+    public void deleteUser(String username) {
+        adminAccountRepository.deleteById(username);
+    }
+
+}
